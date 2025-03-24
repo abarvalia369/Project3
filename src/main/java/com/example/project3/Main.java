36fd3ba774de019e483bbb6712791eb30518a513
Arpeet Barvalia <arpeetbarvalia@gmail.com>
2025-03-23 21:30:30 -0400

    added tabs for open new, etc.

--- src/main/java/com/example/project3/Main.java
+++ src/main/java/com/example/project3/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("RU Bank - Transaction Manager");
         stage.setScene(scene);
         stage.show();
